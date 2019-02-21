b3061cc8daab508cca8a3f9a6d36312cb88529fc
Nicolas GAYE <nicolas.gaye_ssii@i-bp.fr>
2019-02-21 10:14:43 +0100

    Add queries by id

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,16 +2,31 @@ function characters(parent, args, context) {
   return context.prisma.characters();
 }
 
+function character(parent, args, context) {
+  return context.prisma.character({id: args.id});
+}
+
 function places(parent, args, context) {
   return context.prisma.places();
+}
+
+function place(parent, args, context) {
+  return context.prisma.place({id: args.id});
 }
 
 function events(parent, args, context) {
   return context.prisma.events();
 }
 
+function event(parent, args, context) {
+  return context.prisma.event({id: args.id});
+}
+
 module.exports = {
   characters,
+  character,
   places,
-  events
+  place,
+  events,
+  event
 };
